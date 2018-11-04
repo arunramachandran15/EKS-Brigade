6862db5d8805c14af8919dc5ad025256d1238da5
Ubuntu <ubuntu@ip-192-168-225-105.us-west-2.compute.internal>
2018-11-04 05:06:37 +0000

    real brigade

--- brigade.js
+++ brigade.js
@@ -1,7 +1,65 @@
-const { events } = require("brigadier");
+const { events, Job } = require("brigadier");
 
+events.on("push", (e, project) => {
+  console.log("received push for commit " + e.commit)
 
+  var testJob = new Job("test-runner")
 
-events.on("push", function(e, project) {
-  console.log("received push for commit " + e.revision.commit)
+  testJob.image = "python:3"
+
+  testJob.tasks = [
+    "cd /src/",
+    "pip install -r requirements.txt",
+    "cd /src/",
+    "python setup.py test"
+  ]
+
+  testJob.run().then( () => {
+    events.emit("test-done", e, project)
+  })
 })
+
+events.on("test-done", (e, project) => {
+  console.log("Building docker image")
+
+  var dockerBuild = new Job("docker-build")
+
+  dockerBuild.image = "docker:dind"
+  dockerBuild.privileged = true;
+
+  dockerBuild.env = {
+    DOCKER_DRIVER: "overlay"
+  }
+
+  dockerBuild.env.DOCKER_USER = project.secrets.dockerLogin
+  dockerBuild.env.DOCKER_PASS = project.secrets.dockerPass
+
+  dockerBuild.tasks = [
+    "dockerd-entrypoint.sh &",
+    "sleep 20",
+    "cd /src/",
+    "docker build -t tettaji/brigade-test:latest .",
+    "docker login -u $DOCKER_USER -p $DOCKER_PASS",
+    "docker push tettaji/brigade-test:latest"
+  ]
+
+  dockerBuild.run().then( () => {
+    events.emit("build-done", e, project)
+  })
+})
+
+events.on("build-done", (e, project) => {
+  console.log("Deploying to cluster")
+
+  var deploy = new Job("deploy-runner", "tettaji/kubectl:1.9.0")
+
+  deploy.tasks = [
+    "cd /src",
+    "kubectl apply -f deploy.yml"
+  ]
+
+  deploy.run().then( () => {
+    events.emit("success", e, project)
+  })
+})
+
